a0f46f9040ac22a1cecd54b08f492cd854da3661
Laila Chokri <lchokri@e2r11p12.1337.ma>
2023-04-05 15:51:18 +0000

    ..

--- srcs/requirements/wordpress/tools/wp-cli_install.sh
+++ srcs/requirements/wordpress/tools/wp-cli_install.sh
@@ -1,9 +1,7 @@
 
 cd /var/www/html/wordpress
 wp core download --allow-root 
-wp config create --allow-root --dbname=lala  --dbuser=lolo --dbpass=ddd --dbhost=mariadb
-#--dbname=${MYSQL_DATABASE}  --dbuser=${MYSQL_USER} --dbpass=${MYSQL_PASSWORD} --dbhost=mariadb
-wp core install --allow-root --title=Inception --admin_user=elsie wpcli --admin_password=elsiepass  --admin_email=email
-#wp core install  --title=Inception --admin_user=${WW_USER} wpcli --admin_password=${WP_PW}  --admin_email=${WP_EMAIL}
+wp config create --allow-root --dbname=${MYSQL_DATABASE}  --dbuser=${MYSQL_USER} --dbpass=${MYSQL_PASSWORD} --dbhost=$(DB_HOST)
+wp core install --allow-root --title=Inception --admin_user=${WW_USER} wpcli --admin_password=${WP_PW}  --admin_email=${WP_EMAIL}
 
 exec $@
