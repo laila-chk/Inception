0e6a6127bf495dea3d8bbe02a396bc852429caf8
Laila Chokri <lchokri@e2r8p11.1337.ma>
2023-04-09 00:37:56 +0000

    nors is a life saver

--- srcs/requirements/mariadb/tools/mdb_start.sh
+++ srcs/requirements/mariadb/tools/mdb_start.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
-service mysql start
-mysql -u root -e "CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE ; CREATE USER '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD' ; GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%'; FLUSH PRIVILEGES;"
+mkdir -p /run/mysqld/
 
-#service mysql stop
+chown mysql:mysql /run/mysqld
+
+service mysql start
+mysql <<< "CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE ; CREATE USER IF NOT EXISTS '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD' ; GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%'; FLUSH PRIVILEGES;"
+
+service mysql stop
 
 #exec bash
-mysqld
-# --bind-address=0.0.0.0
+mysqld --bind-address=0.0.0.0
--- srcs/requirements/wordpress/tools/wp-cli_install.sh
+++ srcs/requirements/wordpress/tools/wp-cli_install.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 cd /var/www/html/wordpress
-wp core download --allow-root
-rm -rf wp-config.php
-wp config create --allow-root --dbname=${MYSQL_DATABASE}  --dbuser=${MYSQL_USER} --dbpass=${MYSQL_PASSWORD} --dbhost=${MYSQL_HOST}
-wp core install --allow-root --title=Inception --admin_user=${WW_USER} wpcli --admin_password=${WP_PW}  --admin_email=${WP_EMAIL}
-
 mkdir -p /run/php
+wp core download --allow-root
+wp config create --allow-root --dbname=${MYSQL_DATABASE}  --dbuser=${MYSQL_USER} --dbpass=${MYSQL_PASSWORD} --dbhost=${MYSQL_HOST}
+wp core install --allow-root --url=${DOMAIN} --title=Inception --admin_user=${WW_USER} --admin_password=${WP_PW}  --admin_email=${WP_EMAIL} --skip-email
+
+
 sed -i 's/\/run\/php\/php7\.3-fpm\.sock/wordpress:9000/g' /etc/php/7.3/fpm/pool.d/www.conf
 
 
-exec $@
+php-fpm7.3 -R -F
