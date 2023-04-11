5161ca74c61684455a103f5d62a8c8f23be52569
Laila Chokri <lchokri@e2r8p12.1337.ma>
2023-04-11 18:51:54 +0000

    added makefile

--- Makefile
+++ Makefile
@@ -8,13 +8,15 @@ down:
 	docker compose -f srcs/docker-compose.yml down 
 
 clean:
-	docker rmi $$(docker images -aq)	
+	docker rmi -f $$(docker images -aq)	
 
 fclean:
-	docker rm $$(docker ps -aq)	
+	docker rm -f $$(docker ps -aq)	
 	
 rm_vols:
-	docker volume prune 
+	 docker volume rm $$(docker volume ls -q)
 
 cclean:
-	docker system prune -a
+	docker system prune -af
+
+super_clean: down cclean  clean fclean rm_vols 
--- srcs/requirements/wordpress/tools/wp-cli_install.sh
+++ srcs/requirements/wordpress/tools/wp-cli_install.sh
@@ -3,8 +3,8 @@ cd /var/www/html/wordpress
 mkdir -p /run/php
 wp core download --allow-root
 wp config create --allow-root --dbname=${MYSQL_DATABASE}  --dbuser=${MYSQL_USER} --dbpass=${MYSQL_PASSWORD} --dbhost=${MYSQL_HOST}
-wp core install --allow-root --url=${DOMAIN} --title=Inception --admin_user=${WW_USER} --admin_password=${WP_PW}  --admin_email=${WP_EMAIL} --skip-email
-
+wp core install --allow-root --url=${DOMAIN} --title=Inception --admin_user=${WPA_USER} --admin_password=${WPA_PW}  --admin_email=${WPA_EMAIL} --skip-email
+wp user create --allow-root $WW_USER $WP_EMAIL --user_pass=$WP_PW 
 
 sed -i 's/\/run\/php\/php7\.3-fpm\.sock/wordpress:9000/g' /etc/php/7.3/fpm/pool.d/www.conf
 
