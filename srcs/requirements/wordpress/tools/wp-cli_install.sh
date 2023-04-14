548e85bce577d0a0e914adca1932af3f43f39ff7
laila-chk <chokri.laila.ch@gmail.com>
2023-04-13 20:18:00 -0400

    removed unnecessary from wordpress/

--- srcs/requirements/wordpress/tools/wp-cli_install.sh
+++ srcs/requirements/wordpress/tools/wp-cli_install.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 cd /var/www/html/wordpress
-mkdir -p /run/php
 wp core download --allow-root
 wp config create --allow-root --dbname=${MYSQL_DATABASE}  --dbuser=${MYSQL_USER} --dbpass=${MYSQL_PASSWORD} --dbhost=${MYSQL_HOST}
 wp core install --allow-root --url=${DOMAIN} --title=Inception --admin_user=${WPA_USER} --admin_password=${WPA_PW}  --admin_email=${WPA_EMAIL} --skip-email
 wp user create --allow-root $WW_USER $WP_EMAIL --user_pass=$WP_PW 
 
 sed -i 's/\/run\/php\/php7\.3-fpm\.sock/wordpress:9000/g' /etc/php/7.3/fpm/pool.d/www.conf
-
 
 php-fpm7.3 -R -F
